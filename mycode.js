2f0f0fe2cbaf0dd24da85c8d9b7b2d97843222ee
sarulatha-Hub <sarulatha.naasai@gmail.com>
2026-03-28 11:55:08 +0530

    My html projects

---
+++ mycode.js
@@ -0,0 +1,26 @@
+//single quote and double quote
+
+//let funActivity = 'Let's learn javascript';
+let funActivity = "Let's learn JavaScript";
+console.log(funActivity);
+//  let question = "Do you want to learn JavaScript? "Yes!"";
+let question = 'Do you want to learn JavaScript? "Yes!"';
+console.log(question);
+
+//let funActivity = 'Let\'s learn javascript';// this escape character ;
+//  let question = "Do you want to learn JavaScript? \"Yes!\"";
+
+
+//backtick
+ let language = "JavaScript";
+ let message = `Let's learn ${language}`;
+ console.log(message);
+
+ 
+
+console.log("Project module loaded.");
+let a = 5,
+  b = 6;
+console.log(a + b);
+//alert(a+b); this show the alert box with the message
+document.getElementById("demo").innerHTML = a + b;
